62bad907574843f2deb663688a1d978f67590dc6
Martin <m87.wheeler@gmail.com>
2020-09-04 10:08:21 +0100

    Update Gatsby Manifest
    Favicon and Theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ripple Fitness`,
+        short_name: `Ripple`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `##4173af`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/icons/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
     {
