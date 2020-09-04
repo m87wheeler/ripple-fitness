76197ebb1ffc929d1f4c3e739e8f8901b567fc9b
Martin <m87.wheeler@gmail.com>
2020-09-04 10:32:40 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,36 @@
 import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout/Layout"
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
+
+const NotFoundContainer = styled.div`
+  padding-top: 3rem;
+  text-align: center;
+
+  h1 {
+    color: ${props => props.theme.colors.secondary};
+  }
+
+  p {
+    color: ${props => props.theme.colors.black};
+
+    a {
+      color: ${props => props.theme.colors.primary};
+    }
+  }
+`
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    {/* <SEO title="404: Not found" /> */}
+    <NotFoundContainer>
+      <h1>NOT FOUND</h1>
+      <p>
+        This page doesn't exist yet. Why not <Link to="/">go back</Link>?
+      </p>
+    </NotFoundContainer>
   </Layout>
 )
 
